cd8156cbd8aab060320657215e8ecfb136dbe27b
sruthi-lal <lalsruthi145@gmail.com>
2024-05-30 18:21:19 +0530

    javascript data type

---
+++ js/data-types.js
@@ -0,0 +1,28 @@
+let num = 1.234
+console.log(num)
+
+let nums = 1e6;
+console.log(nums)
+
+let bigint =  12345678901234567890n;
+
+let anotherbigint =  98765432109876543210n;
+
+let calbigint = bigint + anotherbigint;
+console.log(calbigint);
+
+let convertnum = BigInt(100);
+let bigintNum = Number(convertnum)
+ 
+console.log(bigintNum);
+
+//To string
+let Numbers = 12344;
+let toStrings  = Numbers.toString()
+console.log(toStrings);
+
+//String to number
+
+let thestring = "123456"
+let toNum =  Number(thestring);
+console.log(toNum)
