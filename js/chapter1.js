bc89c7a815b87aae81ddaf52884f34e9cbc612fc
sruthi-lal <lalsruthi145@gmail.com>
2024-05-30 13:04:11 +0530

    adding let-const-var

--- js/chapter1.js
+++ js/chapter1.js
@@ -1,8 +1,27 @@
-function welcome(){
-       let names = "Sruthi";
-       console.log(names)
-    }
+// Let Example 1
 
-    welcome()
-console.log(names);
+let x = 10;
+if (x === 10) {
+    let x = 20;
+    console.log(x);
+}
+console.log(x);
 
+
+// Example with var 
+
+var y = 40;
+if (y === 40) {
+    var y = 60;
+    console.log(y);
+}
+console.log(y);
+
+//Example with Const
+
+const z = 100;
+if (z === 100) {
+    const z = 200;
+    console.log(z)
+}
+console.log(z)
