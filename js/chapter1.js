70a927c2736c0bd6ecfaba09d3edbdacc8f055cd
sruthi-lal <lalsruthi145@gmail.com>
2024-06-10 12:07:41 +0530

    some more examples

--- js/chapter1.js
+++ js/chapter1.js
@@ -25,3 +25,37 @@ if (z === 100) {
     console.log(z)
 }
 console.log(z)
+
+
+//10TH june var
+ function chaptervar(){
+    var x = 10;
+     if(x === 10){
+        var x = 20;
+        console.log(x)
+    }
+    console.log(x)
+ }
+ chaptervar()
+
+//10TH june let
+ function chapterlet(){
+    let y= 40;
+    if(y === 40){
+        let y = 10;
+        console.log(y)
+    }
+    console.log(y)
+ }
+ chapterlet()
+ 
+//10TH june const
+ function chapterconst(){
+    const n = 22;
+    if(n === 22){
+        const n = 44;
+        console.log(n)
+    }
+    console.log(n)
+ }
+ chapterconst()
